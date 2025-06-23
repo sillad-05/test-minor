b838d3507e7a3393b59e2aa14241ba4d8e27d255
sillad-05 <kumaralok9233@gmail.com>
2025-06-23 22:57:54 +0530

    Add files via upload

---
+++ loop.py
@@ -0,0 +1,18 @@
+# l=["apple","mango","orange","banana","grapes"]
+# for i in l:
+#     print(i)
+# i=5
+# for x in range(1,11):
+#     print(i,"X",x,"=",i*x)
+
+
+# i=1
+# x=int(input("enter a no: "))
+# while(i<11):
+#     print(x,"X",i,"=",x*i)
+#     i+=1
+
+# for i in range(11):
+#     if(i%2!=0):
+#         continue
+#     print(i)
